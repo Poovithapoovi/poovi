ec772e893ddedb512f5bc8cab263a7ebcb292c7d
Poovithapoovi <poovikailash@gmail.com>
2017-06-08 17:23:25 +0530

    reverse.java

---
+++ reverse.java
@@ -0,0 +1,13 @@
+
+import java.io.*;
+import java.util.*;
+
+public class reverseS {
+ public static void main(String[] args) {
+  Scanner a=new Scanner(System.in);
+  String input=a.nextLine();
+ StringBuilder input1= new StringBuilder();
+ input1.append(input);
+  input1=input1.reverse(); 
+  System.out.print(input1);
+ }}
