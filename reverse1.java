fa731b4e8b7ca391e1473d6d0ca3b5241348ebc8
Poovithapoovi <poovikailash@gmail.com>
2017-08-22 22:19:29 +0530

    Rename reverse.java to reverse1.java

---
+++ reverse1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class reverse{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.next();
+StringBuffer str1=new StringBuffer(str);
+str1.reverse();
+System.out.print(str1);}}
