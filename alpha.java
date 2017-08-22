1c05e2e363230b4e43b94dc036b389216df0f4b5
Poovithapoovi <poovikailash@gmail.com>
2017-08-22 21:29:01 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class alpha
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+Char str=sc.next().chatAt(0);
+if(Character.isLetter(str))
+{
+System.out.print("It is a character");}
+else
+{
+System.out.print("It is not a character");}}}
