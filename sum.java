08343913ac4233c660d7cd66f64200a46f9a5c00
Poovithapoovi <poovikailash@gmail.com>
2017-08-22 22:00:58 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class sum{
+public static void main(String args []){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int sum=0;
+for(int i=0;i<=a;i++)
+{
+sum=sum+i;}
+System.out.print( "the sum is"+sum);}}
