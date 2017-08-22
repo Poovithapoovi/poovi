8e92e52314a457192f192e928aaa864ec2aa5d5a
Poovithapoovi <poovikailash@gmail.com>
2017-08-22 21:16:17 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class vowel
+{
+public static void main(String args[])
+{
+   Scanner sc=new Scanner(System.in);
+   String a=sc.next();
+   if((a=="a")||(a=="e")||(a=="i")||(a=="o")||(a=="u"))
+   {
+   System.out.print("the character is a vowel");
+   }
+   else
+   {
+   System.ot.print("The character is a consonant");
+   }}}
