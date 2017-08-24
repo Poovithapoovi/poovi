e4c721be52530f21fa76e5539674a46699946551
Poovithapoovi <poovikailash@gmail.com>
2017-08-24 19:31:04 +0530

    Create strint.java

---
+++ strint.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class strint{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+String str=sc.next();
+int a=Integer.parseInt(str);
+System.out.print(a);}}
