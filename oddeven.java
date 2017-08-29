6d804ca5b7d318477c9bdb7d3865ff364ebd2757
Poovithapoovi <poovikailash@gmail.com>
2017-08-29 21:16:29 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -5,9 +5,14 @@ Scanner sc=new Scanner(System.in);
 String str=sc.next();
 StringBuffer str1=new StringBuffer(str);
 int len=str.length();
+char b=str.charAt(0);
 char a[]=str.toCharArray();
+String str2;
 if((len%2)==0){
 str1.reverse();
 System.out.print(str1);}
 else{
-}}
+    str1=str1.delete(0,1);
+    str2=str1.toString();
+    System.out.print(str2+b);
+}}}
