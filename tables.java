440b5aeab34505b107b98ef212a1461ec003eae1
Poovithapoovi <poovikailash@gmail.com>
2017-08-29 11:14:07 +0530

    Create tables.java

---
+++ tables.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class tables{
+public static void main(String args []){
+int n;
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+for(int i=1;i<=b;i++){
+n=a*i;
+System.out.print(a+"*"+i+"="+n);}}}
