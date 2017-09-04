e11b6079c74ce95e08be6524a699ca7e4bee6936
Poovithapoovi <poovikailash@gmail.com>
2017-09-04 21:12:30 +0530

    Create primer.java

---
+++ primer.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class primer{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+int a=sc.nextInt();
+int count=0;
+System.out.print("2 3 ");
+for(int i=4;i<=x;i++){
+int n=i/2;
+for(int j=2;j<n;j++){
+if(i!=j){
+if(i%j==0){
+count++;
+}}}if(count==0){
+System.out.print(i+" ");}}}}
