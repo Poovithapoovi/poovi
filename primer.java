2c841b89b4d535ab947c284ebb4bfc73ee08af78
Poovithapoovi <poovikailash@gmail.com>
2017-09-04 21:15:15 +0530

    Update primer.java

--- primer.java
+++ primer.java
@@ -2,10 +2,11 @@ import java.util.*;
 class primer{
 public static void main(String args[]){
 Scanner sc= new Scanner(System.in);
-int a=sc.nextInt();
+int x=sc.nextInt();
+  int b=sc.nextInt();
 int count=0;
 System.out.print("2 3 ");
-for(int i=4;i<=x;i++){
+for(int i=x;i<=b;i++){
 int n=i/2;
 for(int j=2;j<n;j++){
 if(i!=j){
