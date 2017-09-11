c810960b7ea2cc4b8db8932ed004e9f96d7487eb
Poovithapoovi <poovikailash@gmail.com>
2017-09-11 09:45:07 +0530

    Create star.java

---
+++ star.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class star{
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+int a=sc.nextInt();
+for(int i=a*2;i>=0;i--){
+for(int i=0;i<i;j++){
+if(i=a/2){
+System.out.print(" ");}
+System.out.print("*");}
